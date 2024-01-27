06f803e67f874b843d5eae7c1327a794595291dd
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-01-27 21:07:27 +0330

    responsiveness function in the Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,23 @@ import Island from '../models/Island'
 
 
 const Home = () => {
+    const adjustIslandForScreenSize = () =>{
+        let screenScale = null;
+        let screenPosition = [0,-6.5,-43];
+        let rotation = [0.1, 4.7, 0];
+
+        if(window.innerWidth<768){
+            screenScale = [0.9,0.9,0.9];
+        }else{
+            screenScale=[1,1,1];
+        }
+
+        return [screenScale,screenPosition, rotation];
+    }
+
+    const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
+
   return (
     <section className="w-full h-screen relative">
         <Canvas 
@@ -22,7 +39,11 @@ const Home = () => {
                 <spotLight />
                 <hemisphereLight />
 
-                <Island />
+                <Island
+                    scale={islandScale}
+                    position={islandPosition}
+                    rotation={ islandRotation}
+                />
             </Suspense>
         </Canvas>
     </section>
