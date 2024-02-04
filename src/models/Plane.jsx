2d6ee639d6c116c1bbc0ce9b7cad3404cec249e6
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 09:56:25 +0330

    changing the direction of the plane

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import planeScene from '../assets/3d/plane.glb'
 import { useGLTF } from '@react-three/drei'
 
-const Plane = () => {
+const Plane = ({ isRotating, ...props }) => {
     const { scene, animations } = useGLTF(planeScene);
     return (
-        <mesh>
+        <mesh {...props}>
             <primitive object={scene} />
         </mesh>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -28,7 +28,23 @@ const Home = () => {
         return [screenScale, screenPosition, rotation];
     }
 
+    const adjustPlaneForScreenSize = () => {
+        let screenScale, screenPosition;
+
+        if (window.innerWidth < 768) {
+            screenScale = [1.5, 1.5, 1.5];
+            screenPosition = [0, -1.5, 0];
+        } else {
+            screenScale = [3, 3, 3];
+            screenPosition = [0, -4, -4];
+        }
+
+        return [screenScale, screenPosition];
+    }
+
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
+    const [planeScale, planePosition] = adjustPlaneForScreenSize();
 
 
     return (
@@ -51,7 +67,12 @@ const Home = () => {
                         isRotating={isRotating}
                         setIsRotating={setIsRotating}
                     />
-                    <Plane />
+                    <Plane
+                        planeScale={planeScale}
+                        planePosition={planePosition}
+                        isRotating={isRotating}
+                        rotation={[0, 20, 0]}
+                    />
                 </Suspense>
             </Canvas>
         </section>
