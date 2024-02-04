862b4c51e6b5265dc4fa4757e2317cffb24f06cf
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 09:09:56 +0330

    adding Bird model

---
+++ src/models/Plane.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Plane = () => {
+    return (
+        <mesh>Plane</mesh>
+    )
+}
+
+export default Plane
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { Canvas } from "@react-three/fiber"
-import {Suspense } from 'react'
+import { Suspense } from 'react'
 import Loader from "../components/Loader"
 import Island from '../models/Island'
 import Sky from "../models/Sky"
+import Bird from "../models/Bird"
+import Plane from "../models/Plane"
 
 {/* <div className="absolute top-28 right-0 left-0 z-10 flex items-center justify-center">
     POPUP
@@ -10,44 +12,46 @@ import Sky from "../models/Sky"
 
 
 const Home = () => {
-    const adjustIslandForScreenSize = () =>{
+    const adjustIslandForScreenSize = () => {
         let screenScale = null;
-        let screenPosition = [0,-6.5,-43];
+        let screenPosition = [0, -6.5, -43];
         let rotation = [0.1, 4.7, 0];
 
-        if(window.innerWidth<768){
-            screenScale = [0.9,0.9,0.9];
-        }else{
-            screenScale=[1,1,1];
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
         }
 
-        return [screenScale,screenPosition, rotation];
+        return [screenScale, screenPosition, rotation];
     }
 
     const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
 
 
-  return (
-    <section className="w-full h-screen relative">
-        <Canvas 
-            className="w-full h-screen bg-transparent"
-            camera={{near: 0.1, far: 1000}}
-        >
-            <Suspense fallback={<Loader />}>
-                <directionalLight position={[12, 4, -3]} intensity={2} />
-                <ambientLight intensity={0.5} />
-                <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
+    return (
+        <section className="w-full h-screen relative">
+            <Canvas
+                className="w-full h-screen bg-transparent"
+                camera={{ near: 0.1, far: 1000 }}
+            >
+                <Suspense fallback={<Loader />}>
+                    <directionalLight position={[12, 4, -3]} intensity={2} />
+                    <ambientLight intensity={0.5} />
+                    <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-                <Sky />
-                <Island
-                    scale={islandScale}
-                    position={islandPosition}
-                    rotation={ islandRotation}
-                />
-            </Suspense>
-        </Canvas>
-    </section>
-  )
+                    <Bird />
+                    <Sky />
+                    <Island
+                        scale={islandScale}
+                        position={islandPosition}
+                        rotation={islandRotation}
+                    />
+                    <Plane />
+                </Suspense>
+            </Canvas>
+        </section>
+    )
 }
 
 export default Home
