8820c4de3402b51aa134c687fec4270deff16c57
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 10:23:38 +0330

    functions that needed for the logic of rotation of the island when the mouse is clicked by the user

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -4,9 +4,54 @@ import { useFrame, useThree } from "@react-three/fiber";
 import islandScene from '../assets/3d/island.glb'
 import { a } from '@react-spring/three'
 
-const Island = (props) => {
+const Island = ({ isRotating, setIsRotating, ...props }) => {
   const islandRef = useRef();
+
+  const { gl, viewport } = useThree();
+
   const { nodes, materials } = useGLTF(islandScene);
+
+  const lastX = useRef(0);
+  const rotationSpeed = useRef(0);
+  const dampingFactor = 0.95;
+
+  const handlePointerDown = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(true);
+
+    const clientX = e.touches
+      ? e.touches[0].clientX
+      : e.clientX;
+
+    lastX.current - clientX;
+  }
+
+  const handlePointerUP = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+    setIsRotating(false);
+
+    const clientX = e.touches
+      ? e.touches[0].clientX
+      : e.clientX;
+
+    const delta = (clientX - lastX.current)
+      / viewport.width;
+
+    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+    lastX.current = clientX;
+    rotationSpeed.current = delta * 0.01 * Math.PI;
+  }
+
+  const handlePointerMove = (e) => {
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (isRotating) handlePointerUP(e);
+  }
+
+
   return (
     <a.group ref={islandRef} {...props}>
       <mesh
