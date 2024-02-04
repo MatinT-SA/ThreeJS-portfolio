598686d86dfd4cdfcb912a67123f49eacb54879b
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 10:41:32 +0330

    useFrame hook

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -27,28 +27,28 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     lastX.current - clientX;
   }
 
-  const handlePointerUP = (e) => {
+  const handlePointerUp = (e) => {
     e.stopPropagation();
     e.preventDefault();
     setIsRotating(false);
-
-    const clientX = e.touches
-      ? e.touches[0].clientX
-      : e.clientX;
-
-    const delta = (clientX - lastX.current)
-      / viewport.width;
-
-    islandRef.current.rotation.y += delta * 0.01 * Math.PI;
-    lastX.current = clientX;
-    rotationSpeed.current = delta * 0.01 * Math.PI;
   }
 
   const handlePointerMove = (e) => {
     e.stopPropagation();
     e.preventDefault();
 
-    if (isRotating) handlePointerUP(e);
+    if (isRotating) {
+      const clientX = e.touches
+        ? e.touches[0].clientX
+        : e.clientX;
+
+      const delta = (clientX - lastX.current)
+        / viewport.width;
+
+      islandRef.current.rotation.y += delta * 0.01 * Math.PI;
+      lastX.current = clientX;
+      rotationSpeed.current = delta * 0.01 * Math.PI;
+    };
   }
 
   const handleKeyDown = (e) => {
@@ -67,21 +67,55 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
     }
   }
 
+  useFrame(() => {
+    if (!isRotating) {
+      rotationSpeed.current *= dampingFactor;
+
+      if (Math.abs(rotationSpeed.current) < 0.001) {
+        rotationSpeed.current = 0;
+      }
+    } else {
+      const rotation = islandRef.current.rotation.y;
+
+      const normalizedRotation =
+        ((rotation % (2 * Math.PI)) + 2 * Math.PI) % (2 * Math.PI);
+
+      // Set the current stage based on the island's orientation
+      switch (true) {
+        case normalizedRotation >= 5.45 && normalizedRotation <= 5.85:
+          setCurrentStage(4);
+          break;
+        case normalizedRotation >= 0.85 && normalizedRotation <= 1.3:
+          setCurrentStage(3);
+          break;
+        case normalizedRotation >= 2.4 && normalizedRotation <= 2.6:
+          setCurrentStage(2);
+          break;
+        case normalizedRotation >= 4.25 && normalizedRotation <= 4.75:
+          setCurrentStage(1);
+          break;
+        default:
+          setCurrentStage(null);
+      }
+    }
+  })
+
   useEffect(() => {
-    document.addEventListener('pointerdown', handlePointerDown);
-    document.addEventListener('pointerup', handlePointerUp);
-    document.addEventListener('pointermove', handlePointerMove);
+    const canvas = gl.domElement;
+    canvas.addEventListener('pointerdown', handlePointerDown);
+    canvas.addEventListener('pointerup', handlePointerUp);
+    canvas.addEventListener('pointermove', handlePointerMove);
     document.addEventListener('keydown', handleKeyDown);
     document.addEventListener('keyup', handleKeyUp);
 
     return () => {
-      document.removeEventListener('pointerdown', handlePointerDown);
-      document.removeEventListener('pointerup', handlePointerUP);
-      document.removeEventListener('pointermove', handlePointerUP);
+      canvas.removeEventListener('pointerdown', handlePointerDown);
+      canvas.removeEventListener('pointerup', handlePointerUp);
+      canvas.removeEventListener('pointermove', handlePointerUp);
       document.removeEventListener('keydown', handleKeyDown);
       document.removeEventListener('keyup', handleKeyUp);
     }
-  }, [gl, handlePointerDown, handlePointerUP, handlePointerMove])
+  }, [gl, handlePointerDown, handlePointerUp, handlePointerMove])
 
 
   return (
