13140038d2d02c190abc80f5d8bf7c4f3ec55108
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 10:43:54 +0330

    making our mouse moving more smooth

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -74,6 +74,8 @@ const Island = ({ isRotating, setIsRotating, ...props }) => {
       if (Math.abs(rotationSpeed.current) < 0.001) {
         rotationSpeed.current = 0;
       }
+
+      islandRef.current.rotation.y += rotationSpeed.current;
     } else {
       const rotation = islandRef.current.rotation.y;
 
