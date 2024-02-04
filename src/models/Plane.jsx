e1071a9eb7d971e5c10a0f85feec268a555fc9d4
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-04 09:13:42 +0330

    adding plane model

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import planeScene from '../assets/3d/plane.glb'
+import { useGLTF } from '@react-three/drei'
 
 const Plane = () => {
+    const { scene, animations } = useGLTF(planeScene);
     return (
-        <mesh>Plane</mesh>
+        <mesh>
+            <primitive object={scene} />
+        </mesh>
     )
 }
 
