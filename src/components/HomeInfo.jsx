9e48841265a91bc250758a70d1d6cf8552824d49
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-02-11 19:30:42 +0330

    adding the pop-up box for our introduction

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
-const HomeInfo = () => {
-  return (
-    <div>HomeInfo</div>
-  )
+const renderContent ={
+    1: (
+        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue
+        py-4 px-8 text-white mx-5'>
+            Hi, I am <span className='font-semibold'>Matin</span>
+            <br />
+            A Web Developer from Iran
+        </h1>
+    ),
+    2: (
+        <h1>2</h1>
+    ),
+    3: (
+        <h1>3</h1>
+    ),
+    4: (
+        <h1>4</h1>
+    ),
+}
+
+const InfoBox = ({text, link, btnText})=>(
+    <div className='info-box'>
+        {text}
+    </div>
+)
+
+const HomeInfo = ({currentStage}) => {
+  return renderContent[currentStage] || null
 }
 
 export default HomeInfo
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
         let screenPosition = [0, -6.5, -43];
-        let rotation = [0.1, 4.7, 0];
+        let rotation = [0.1, -1.5, 0];
 
         if (window.innerWidth < 768) {
             screenScale = [0.9, 0.9, 0.9];
