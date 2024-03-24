cc8d407566d48182758021c398f4d4e6dbdc8c13
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-03-24 12:08:33 +0330

    creating our custom hook for alert plus giving our Alert component some styles

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,12 +4,16 @@ import { Canvas } from '@react-three/fiber';
 
 import {Fox} from '../models/Fox';
 import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false);
   const [currentAnimation, setCurrentAnimation] = useState('idle')
+
+  const {alert, showAlert, hideAlert} = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value })
@@ -32,10 +36,10 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
       setIsLoading(false);
-      // TODO: Show success message
-      // TODO: Hide an alert
+      showAlert({show: true, text: 'Message sent successfully', type: 'success'});
 
       setTimeout(() => {
+        hideAlert();
         setCurrentAnimation('idle') 
         setForm({ name: '', email: '', message: '' });
       }, [3000]);
@@ -44,7 +48,7 @@ const Contact = () => {
       setIsLoading(false);
       setCurrentAnimation('idle');
       console.log(error);
-      // TODO: show error message
+      showAlert({show: true, text:'Unfortunately, it failed to be sent', type: 'danger'})
     })
   };
   const handleFocus = () => setCurrentAnimation('walk');
@@ -52,6 +56,9 @@ const Contact = () => {
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+      <Alert {...alert} />
+
       <div className="flex-1 min-w-[50] flex flex-col">
         <h1 className="head-text">Get in touch</h1>
 
