586dbc8fae5c905816de29e0c103fe5c1c3dc6ee
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-03-30 22:41:47 +0330

    styling logos [about] - alignments and styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,4 @@
-import {skills} from '../constants';
+import { skills } from '../constants';
 
 const About = () => {
   return (
@@ -14,9 +14,10 @@ const About = () => {
         <h3 className="subhead-text">My Skills</h3>
 
         <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill)=>(
-            <div>
-              <div>
+          {skills.map((skill) => (
+            <div className='block-container w-20 h-20'>
+              <div className='btn-back rounded-xl' />
+              <div className='btn-front rounded-xl flex justify-center items-center'>
                 <img src={skill.imageUrl} alt={skill.name} className='w-1/2 h-1/2 object-contain' />
               </div>
             </div>
