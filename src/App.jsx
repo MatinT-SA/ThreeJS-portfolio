66addb49c2dc7d2820898ec7fd0938350f31e63a
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-01 22:24:08 +0330

    fixing height of app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20">
+    <main className="bg-slate-300/20 h-full">
       <Router>
         <Navbar />
         <Routes>
