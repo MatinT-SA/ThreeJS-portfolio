bc409e8b7cd8d7128884fba3a4636e70bd4bb3e2
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-04 21:27:51 +0330

    add: deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
     base: '/ThreeJS-portfolio',
+    plugins: [react()],
     assetsInclude: ['**/*.glb'],
 })
