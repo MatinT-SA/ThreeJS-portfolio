0b8e4dbda726ccd62ab546cd7ec01983574892b3
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-04 21:40:19 +0330

    removing excessive base from vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/ThreeJS-portfolio',
     plugins: [react()],
     base: '/ThreeJS-portfolio',
     assetsInclude: ['**/*.glb'],
