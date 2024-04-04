bfffdeddfdcc81b29a6193786df2cd3ff517ab11
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-04 20:08:08 +0330

    fixing base url 1

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/ThreeJS-portfolio" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
