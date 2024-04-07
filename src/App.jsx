089742b579e3869e4841fd3bddf51eec61b5d0e7
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-07 18:26:39 +0330

    hashRoute instead of BrowseRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from "react-router-dom";
 import { Home, About, Projects, Contact } from './pages';
 import Navbar from "./components/Navbar";
 
@@ -8,7 +8,7 @@ const App = () => {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/ThreeJS-portfolio" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/ThreeJS-portfolio/about" element={<About />} />
           <Route path="/ThreeJS-portfolio/projects" element={<Projects />} />
           <Route path="/ThreeJS-portfolio/contact" element={<Contact />} />
